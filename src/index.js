7e3657db0a67b81b9915242070b10aae8d982ef9
Shantanu <77541968+shantanu1998s@users.noreply.github.com>
2023-02-07 19:10:22 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+class API {
+	#secure;
+	url;
+	method;
 
+	constructor(url) {
+		this.url = url;
+		this.method = 'GET';
+		this.#secure = url.includes('https');
+	}
+	isSecure() {
+		return this.#secure;
+	}
+	updateUrl(url) {
+		this.url = url;
+		this.#secure = url.includes('https');
+	}
+}
 
-module.exports = { API }
+// const s = new API('http://api.com/api/hello');
+
+module.exports = { API };
